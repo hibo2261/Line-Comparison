ca5a4aa9aaf463a14a8d3f23c4ee25a18b97200c
hibo2261 <hborse1@gmail.com>
2022-08-29 04:14:10 +0530

    [Himanshu]:Taking Input From User

---
+++ src/com/Bridgelabz/LineOfComparison.java
@@ -0,0 +1,28 @@
+package com.Bridgelabz;
+
+import java.util.Scanner;
+
+public class LineOfComparison {
+
+    public static void main(String[] args) {
+        System.out.println("welcome to line comparison computation");
+        /*
+         * UC1- taking input from user
+         */
+        Scanner line = new Scanner(System.in);
+        System.out.println("Enter the value for x1");
+        int x1 = line.nextInt();
+        System.out.println("Enter the value for x2");
+        int x2 = line.nextInt();
+        System.out.println("Enter the value for y1");
+        int y1 = line.nextInt();
+        System.out.println("Enter the value for y2");
+        int y2 = line.nextInt();
+
+        double lenghtOfLine = (Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2)));
+        System.out.println("The length of  Line i.e (x2-x1)+(y2-y1) = " + lenghtOfLine);
+        line.close();
+
+    }
+
+}
