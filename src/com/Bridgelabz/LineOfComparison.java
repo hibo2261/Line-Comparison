1c564a622877fc16dd76ca729b7444bb9ad29551
hibo2261 <hborse1@gmail.com>
2022-09-02 08:10:45 +0530

    [Himanshu]: Added Calculate Difference And Check Equality & Comparison of Lines

--- src/com/Bridgelabz/LineOfComparison.java
+++ src/com/Bridgelabz/LineOfComparison.java
@@ -4,51 +4,53 @@ import java.util.Scanner;
 
 public class LineOfComparison {
 
-    /*
-     * UC2
-     */
-    public static double equalityOfLength() {
+    public static double lengthOfLine1, lengthOfLine2;
+    static int result;
+
+    public double equalityOfLength() {
+        /*
+        Taking inputs from user
+         */
 
         Scanner line = new Scanner(System.in);
 
-        System.out.println("Enter the value for x1 = ");
+        System.out.println("Enter the value for x1");
         double x1 = line.nextDouble();
 
-        System.out.println("Enter the value for x2 = ");
+        System.out.println("Enter the value for x2");
         double x2 = line.nextDouble();
 
-        System.out.println("Enter the value for y1 = ");
+        System.out.println("Enter the value for y1");
         double y1 = line.nextDouble();
 
-        System.out.println("Enter the value for y2 = ");
+        System.out.println("Enter the value for y2");
         double y2 = line.nextDouble();
 
         double length = Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
         return length;
     }
     public static void main(String[] args) {
-        Double lengthOfLine1 = equalityOfLength();
 
-        System.out.println("length of line 1 = " + lengthOfLine1);
+        LineOfComparison line = new  LineOfComparison();
+        Double lengthOfLine1 = line.equalityOfLength();
 
-        Double lengthOfLine2 = equalityOfLength();
+        System.out.println("length of line 1: " + lengthOfLine1);
 
-        System.out.println("lenth of line 2 = " + lengthOfLine2);
+        Double lengthOfLine2 = line.equalityOfLength();
 
-        if(lengthOfLine1.equals(lengthOfLine2)) {
-            System.out.println("Both lines are equal.");
-        }
-        else {
-            System.out.println("Both lines are not equal.");
-        }
-        /*
-         * UC3	comparing 2 lines
-         */
-        if(lengthOfLine1.compareTo(lengthOfLine2) < 0 ) {
-            System.out.println(  lengthOfLine1 +" is lesser than " + lengthOfLine2);
+        System.out.println("lenth of line 2: " + lengthOfLine2);
+
+        System.out.println("euqal:: " + lengthOfLine1.equals(lengthOfLine2));
+        result = Double.compare(lengthOfLine1, lengthOfLine2);
+        if(result < 0 ) {
+            System.out.println(  lengthOfLine1 + " is lesser than "  + lengthOfLine2);
         }
         else {
             System.out.println(lengthOfLine1 + " is greater than "  + lengthOfLine2);
         }
+
+
+
+
     }
 }
