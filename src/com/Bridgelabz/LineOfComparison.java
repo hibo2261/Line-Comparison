cb4012291634b45fd6cd82bb8be2da8b7016454a
hibo2261 <hborse1@gmail.com>
2022-09-02 07:41:07 +0530

    [Himanshu]:Added Checking Equality Of Two Lines

--- src/com/Bridgelabz/LineOfComparison.java
+++ src/com/Bridgelabz/LineOfComparison.java
@@ -1,27 +1,47 @@
 package com.Bridgelabz;
-
 import java.util.Scanner;
+
 
 public class LineOfComparison {
 
-    public static void main(String[] args) {
-        System.out.println("welcome to line comparison computation");
-        /*
-         * UC1- taking input from user
-         */
-        Scanner line = new Scanner(System.in);
-        System.out.println("Enter the value for x1");
-        int x1 = line.nextInt();
-        System.out.println("Enter the value for x2");
-        int x2 = line.nextInt();
-        System.out.println("Enter the value for y1");
-        int y1 = line.nextInt();
-        System.out.println("Enter the value for y2");
-        int y2 = line.nextInt();
+    /*
+     * UC2- check when two lines are equal
+     */
 
-        double lenghtOfLine = (Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2)));
-        System.out.println("The length of  Line i.e (x2-x1)+(y2-y1) = " + lenghtOfLine);
-        line.close();
+    public static double equalityOfLength() {
+
+        Scanner line = new Scanner(System.in);
+
+        System.out.println("Enter the value for x1 = ");
+        double x1 = line.nextDouble();
+
+        System.out.println("Enter the value for x2 = ");
+        double x2 = line.nextDouble();
+
+        System.out.println("Enter the value for y1 = ");
+        double y1 = line.nextDouble();
+
+        System.out.println("Enter the value for y2 = ");
+        double y2 = line.nextDouble();
+
+        double length = Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+        return length;
+    }
+    public static void main(String[] args) {
+        Double lengthOfLine1 = equalityOfLength();
+
+        System.out.println("length of line 1 = " + lengthOfLine1);
+
+        Double lengthOfLine2 = equalityOfLength();
+
+        System.out.println("lenth of line 2 = " + lengthOfLine2);
+
+        if(lengthOfLine1.equals(lengthOfLine2)) {
+            System.out.println("Both lines are equal.");
+        }
+        else {
+            System.out.println("Both lines are not equal.");
+        }
 
     }
 
